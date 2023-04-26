454f2a23e76d402b58566ef46e545a3581e79b19
Andrew DeSimone <36091688+Molarcylinder10@users.noreply.github.com>
2023-04-26 18:29:46 -0500

    Create test.pl

---
+++ test.pl
@@ -0,0 +1,27 @@
+%Part 1: MIN, MAX, AVG
+findMin([Min], Min).
+findMin([H|T], H) :- findMin(T, MT), H < MT.
+findMin([H|T], MT) :- findMin(T, MT), H >= MT.
+
+findMax([Max], Max).
+findMax([H|T], H) :- findMax(T, MT), H > MT.
+findMax([H|T], MT) :- findMax(T, MT), H =< MT.
+
+findSum([], 0).
+findSum([H|T], Sum) :- findSum(T,Tsum), Sum is H + Tsum.
+findAvg(L, Avg) :- findSum(L, Sum), length(L, Len), Avg is Sum/Len.
+
+%Part 2: MODE
+countElem([], _, 0).
+countElem([H|T], H, Hcnt) :- countElem(T, H, THcnt), Hcnt is THcnt + 1.
+countElem([H|T], N, Ncnt) :- H \= N, countElem(T, N, Ncnt).
+
+countElems([], [], []).
+countElems(L, [H], [Hcnt]) :- countElem(L, H, Hcnt).
+countElems(L, [H|T], [Hcnt|TR]) :- countElem(L, H, Hcnt), countElems(L, T, TR).
+findMode(L, M) :- list_to_set(L, U), countElems(L, U, UC), findMaxU(U, UC, M).
+
+%Part 3: MEDIAN
+findMedian(Ls, Med) :- sort(0, @=<, Ls, S), \+isEven(Ls), length(Ls, E), G is floor(E/2), nth0(G, S, Med).
+findMedian(Ls, Med) :- sort(0, @=<, Ls, S), isEven(Ls), length(Ls, E), G is E/2, Z is G-1, nth0(G, S, F), nth0(Z, S, T), findAvg([F, T], Med).
+isEven(Ls) :- length(Ls, N), 0 is mod(N,2).
